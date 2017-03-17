dc802ef4d9d1de2230db29e0666f72fe9d3efc4c
sillyTroll <saradalfalk@gmail.com>
2017-03-17 10:03:11 +0100

    begynnelsen av change data

---
+++ script/changeData.js
@@ -0,0 +1,68 @@
+//Eksempel fra: https://jsfiddle.net/contentful/kefaj4s8/
+//NB: Klasser er foreløpig ikke i bruk!
+
+
+/*-------------- CLIENT --------------*/
+var client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: '59mi8sr8zemv',
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: '3f3d80d1c57594b635592e67231ad92c8bdebffca1a647ae5bca719251fbf059'
+})
+/*-------------- END CLIENT --------------*/
+
+var EVENT_CONTENT_TYPE_ID = 'events';
+
+var container = document.getElementById('content');
+
+/*-------------- GET ENTRIES --------------*/
+client.getEntries({
+    content_type: EVENT_CONTENT_TYPE_ID
+})
+    .then(function (entries) {
+    console.log('Entry Client:', entries.items);
+    container.innerHTML = renderEvents(entries.items);
+})
+/*-------------- END GET ENTRIES --------------*/
+
+/*-------------- GET ALL EVENTS --------------*/
+function renderEvents(events){
+    return '<div class="events">' +
+        events.map(renderSingleEvent).join('\n') +
+        '</div>';
+}
+/*-------------- END GET ALL EVENTS --------------*/
+
+/*-------------- PUT ELEMENTS TOGETHER --------------*/
+function renderSingleEvent(event){
+    var fields = event.fields;
+    return '<div class="eventInList">' +
+                '<div class="eventImage">' +
+                    renderImage(fields.image) +
+                '</div>' +
+
+                '<div class="eventInfo">' +
+                    renderEventInfo(fields) +
+                '</div>' +
+            '</div>';
+}
+/*-------------- PUT ELEMENTS TOGETHER --------------*/
+
+/*-------------- GET DATA FROM ONE EVENT --------------*/
+//This function takes the existing info and put it in to inputfields that kan be edited.
+function renderEventInfo(fields){
+    return '<input type="text" name="title" value="' + fields.title + '">' + '</br>' +
+            '<input type="text" name="whatToExpect size="150" value="' + fields.whatToExpect + '">'
+
+}
+/*-------------- END GET DATA FROM ONE EVENT --------------*/
+
+/*-------------- GET IMAGE --------------*/
+function renderImage(image){
+    if(image && image.fields.file){
+        return '<img src="https://' + image.fields.file.url + '" width="150" height="150"/>';
+    } else {
+    return '';
+    }
+}
+/*-------------- END GET IMAGE --------------*/
