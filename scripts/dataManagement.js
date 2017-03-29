26afd101033699b2df225d9d8ff564388f2a05e4
sillyTroll <saradalfalk@gmail.com>
2017-03-29 13:35:30 +0200

    oops

--- scripts/dataManagement.js
+++ scripts/dataManagement.js
@@ -1,7 +1,7 @@
 var contentful = require('contentful-management')
 var client = contentful.createClient({
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: 'b60f393ec836a43747cb5a238cdc49e379361c7d7a0a96012191fb3745e2532b'
+  accessToken: ''
 })
 
 // This API call will request a space with the specified ID
